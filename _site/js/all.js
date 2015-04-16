f42e5acd2d4238ffd98dac5dbd0847fcc99c4c84
oi11hkm <oi11hkm@cs.umu.se>
2015-04-16 16:13:28 +0200

    font change, buttons

--- _site/js/all.js
+++ _site/js/all.js
@@ -1,12 +1,13 @@
 $(document).on('scroll', function (e) {
-    var alpha = $(document).scrollTop() / 700;
+    var alpha = $(document).scrollTop() / 500;
     $('.navbar-default').css('background-color', 'rgba(255, 255, 255,' + alpha + ')');
 });
 
-$(function(){
+$(document).ready(function(){
         $("#typed").typed({
-            strings: ["Hakc", "Hacking since 1991"],
+            strings: ["Java", "Mobile Applications", "Haskell","Cloud", "Node.js", "Cassandra", "MongoDB"],
             typeSpeed: 100,
-            backDelay: 400
+            backDelay: 400,
+            loop: true
         });
 });
