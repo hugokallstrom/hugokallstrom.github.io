1edcfa02c7d777b77245381674798d435471f741
oi11hkm <oi11hkm@cs.umu.se>
2015-04-22 12:42:17 +0200

    Resume added

--- js/all.js
+++ js/all.js
@@ -4,6 +4,22 @@ $(document).on('scroll', function (e) {
 });
 
 $(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+  if (startchange.length) {
+    $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('background-color', '#f0f0f0');
+      } else {
+          $('.navbar-default').css('background-color', 'transparent');
+      }
+    });
+  }
+});
+
+$(document).ready(function(){
         $("#typed").typed({
             strings: ["Java", "Mobile Applications", "Haskell","Cloud", "Node.js", "Cassandra", "MongoDB"],
             typeSpeed: 100,
