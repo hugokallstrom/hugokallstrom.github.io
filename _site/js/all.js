08ae39cf2ad80607afc7bb69bf0989161e1ce3ef
oi11hkm <oi11hkm@cs.umu.se>
2015-04-28 12:02:23 +0200

    scroll and color

--- _site/js/all.js
+++ _site/js/all.js
@@ -1,29 +1,34 @@
 $(document).on('scroll', function (e) {
     var alpha = $(document).scrollTop() / 500;
     $('.navbar-default').css('background-color', 'rgba(255, 255, 255,' + alpha + ')');
+    $('.fa-angle-down').css('opacity', 0.4-alpha);
 });
 
-$(document).ready(function(){
-   var scroll_start = 0;
-   var startchange = $('#startchange');
-   var offset = startchange.offset();
-  if (startchange.length) {
-    $(document).scroll(function() {
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $(".navbar-default").css('background-color', '#f0f0f0');
-      } else {
-          $('.navbar-default').css('background-color', 'transparent');
-      }
-    });
-  }
+$(document).ready(function (e) {
+  bindMouse();
+  continueScroll();
 });
 
-$(document).ready(function(){
-        $("#typed").typed({
-            strings: ["Java", "Mobile Applications", "Haskell","Cloud", "Node.js", "Cassandra", "MongoDB"],
-            typeSpeed: 100,
-            backDelay: 400,
-            loop: true
-        });
+$(document).ready(function () {
+  $("#typed").typed({
+      strings: ["Java", "Mobile Applications", "Haskell","Cloud", "Node.js", "Cassandra", "MongoDB"],
+      typeSpeed: 100,
+      backDelay: 400,
+      loop: true
+  });
 });
+
+function bindMouse () {
+   $(window).bind("mousewheel", function () {
+    $("html, body").stop();
+  });
+}
+
+function continueScroll () {
+  $('#continue').click(function (e) {
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $('.projects').offset().top - 40
+      }, 800);
+  });
+}
